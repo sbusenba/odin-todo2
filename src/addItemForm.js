611b665ce037254a34d6bf3fcb82e987ff202aa6
Steven Busenbarrick <sbusenba@gmail.com>
2022-05-10 11:01:30 -0700

    Update CSS for addItemForm, aesthetic changes
    Update CSS for addItemForm, changed description box to textarea,
    positioned elements due to aesthetics.

--- src/addItemForm.js
+++ src/addItemForm.js
@@ -1,35 +1,38 @@
 const itemForm = (submitFunction)=>{
 let form = document.createElement('form')
 let legend = document.createElement('legend')
-legend.innerText = 'Add New Task:'
+legend.innerText = 'Add New Task'
 form.appendChild(legend);
 
 let titleLabel = document.createElement('label');
-titleLabel.innerText = 'Title';
+titleLabel.innerText = 'Title:';
 titleLabel.setAttribute('for','titleinput');
 let titleInput =document.createElement('input');
 titleInput.type = 'text';
 titleInput.id= 'titleinput'
+titleLabel.appendChild(titleInput)
 form.appendChild(titleLabel)
-form.appendChild(titleInput)
+
 
 let descLabel = document.createElement('label');
-descLabel.innerText = 'Description';
+descLabel.innerText = 'Description:';
 descLabel.setAttribute('for','description');
-let descInput =document.createElement('input');
+let descInput =document.createElement('textarea');
 descInput.id='description'
-descInput.type = 'text';
+descInput.setAttribute('rows', 3)
+descInput.setAttribute('columns',20)
+descLabel.appendChild(descInput)
 form.appendChild(descLabel)
-form.appendChild(descInput)
+
 
 let dateLabel = document.createElement('label');
-dateLabel.innerText = 'Date';
+dateLabel.innerText = 'Date:';
 dateLabel.setAttribute('for','date');
 let dateInput =document.createElement('input');
 dateInput.id='date';
 dateInput.type = 'date';
+dateLabel.appendChild(dateInput);
 form.appendChild(dateLabel);
-form.appendChild(dateInput);
  
 let addButton = document.createElement('button');
 addButton.textContent = 'Add Item';
